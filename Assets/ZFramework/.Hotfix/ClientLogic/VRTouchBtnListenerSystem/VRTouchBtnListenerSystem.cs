ef074c28db5bdc97d24007a7a3f67860af5387b3
Rucie <76717757@qq.com>
2022-08-17 17:38:47 +0800

    自动给VRPlayer添加UGUI射线支持组件 监听pico摇杆事件  最小化继承VR位移脚本

--- Assets/ZFramework/.Hotfix/ClientLogic/VRTouchBtnListenerSystem/VRTouchBtnListenerSystem.cs
+++ Assets/ZFramework/.Hotfix/ClientLogic/VRTouchBtnListenerSystem/VRTouchBtnListenerSystem.cs
@@ -1,10 +1,13 @@
-﻿using System;
+﻿using KVR_UI;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using UnityEngine;
 using Valve.VR;
+using Valve.VR.InteractionSystem;
+using Valve.VR.Extras;
 
 namespace ZFramework
 {
@@ -16,6 +19,47 @@ namespace ZFramework
             entity.btnB = SteamVR_Input.GetAction<SteamVR_Action_Boolean>("BtnB");
             entity.btnX = SteamVR_Input.GetAction<SteamVR_Action_Boolean>("BtnX");
             entity.btnY = SteamVR_Input.GetAction<SteamVR_Action_Boolean>("BtnY");
+            entity.snapLeftAction = SteamVR_Input.GetBooleanAction("SnapTurnLeft");
+            entity.snapRightAction = SteamVR_Input.GetBooleanAction("SnapTurnRight");
+
+            var player = Player.instance;
+            if (player != null)
+            {
+                entity.snapTurn = player.GetComponentInChildren<SnapTurn>();
+
+                //VRUI
+                var defEventSystem = InputModule.instance;
+                if (defEventSystem != null)
+                {
+                    entity.kvr_InputModule = defEventSystem.gameObject.AddComponent<Kvr_InputModule>();
+                }
+
+                var leftHand = player.leftHand;
+                if (leftHand != null)
+                {
+                    entity.lineLeft =  leftHand.GetComponent<SteamVR_LaserPointer>();
+                    if (entity.lineLeft == null)
+                    {
+                        entity.lineLeft = leftHand.gameObject.AddComponent<SteamVR_LaserPointer>();
+                        entity.lineLeft.color = Color.green;
+                    }
+                    entity.uiPointerLeft = leftHand.gameObject.AddComponent<Kvr_UIPointer>();
+                }
+
+                var rightHand = player.rightHand;
+                if (rightHand != null)
+                {
+                    entity.lineRight = rightHand.GetComponent<SteamVR_LaserPointer>();
+                    if (entity.lineRight == null)
+                    {
+                        entity.lineRight = rightHand.gameObject.AddComponent<SteamVR_LaserPointer>();
+                        entity.lineRight.color = Color.green;
+                    }
+                    entity.uiPointerRight = rightHand.gameObject.AddComponent<Kvr_UIPointer>();
+                }
+
+            }
+
         }
     }
 
@@ -23,19 +67,65 @@ namespace ZFramework
     {
         public override void OnUpdate(VRTouchBtnListenerComponent entity)
         {
-            if (entity.btnA.GetStateDown(SteamVR_Input_Sources.Any) ||
-                entity.btnB.GetStateDown(SteamVR_Input_Sources.Any) ||
-                entity.btnX.GetStateDown(SteamVR_Input_Sources.Any) ||
-                entity.btnY.GetStateDown(SteamVR_Input_Sources.Any))
+            if (entity.btnA != null && entity.btnA.activeBinding && entity.btnA.GetStateDown(SteamVR_Input_Sources.Any))
             {
                 entity.Process.GetComponent<UIComponent>().Show(UIType.Canvas_Menu);
-                //Debug.Log("A");
             }
+            if (entity.btnB != null && entity.btnB.activeBinding && entity.btnB.GetStateDown(SteamVR_Input_Sources.Any))
+            {
+                entity.Process.GetComponent<UIComponent>().Show(UIType.Canvas_Menu);
+            }
+            if (entity.btnX != null && entity.btnX.activeBinding && entity.btnX.GetStateDown(SteamVR_Input_Sources.Any))
+            {
+                entity.Process.GetComponent<UIComponent>().Show(UIType.Canvas_Menu);
+            }
+            if (entity.btnY != null && entity.btnY.activeBinding && entity.btnY.GetStateDown(SteamVR_Input_Sources.Any))
+            {
+                entity.Process.GetComponent<UIComponent>().Show(UIType.Canvas_Menu);
+            }
+
+            if (entity.snapLeftAction != null && entity.snapRightAction != null && entity.snapLeftAction.activeBinding && entity.snapRightAction.activeBinding)
+            {
+                //bool left = entity.snapLeftAction.GetStateDown(SteamVR_Input_Sources.LeftHand) || entity.snapLeftAction.GetStateDown(SteamVR_Input_Sources.RightHand);
+                //bool right = entity.snapRightAction.GetStateDown(SteamVR_Input_Sources.LeftHand) || entity.snapRightAction.GetStateDown(SteamVR_Input_Sources.RightHand);
+                //if (left)
+                //{
+                //    Log.Info("Left");
+                //}
+                //if (right)
+                //{
+                //    Log.Info("Right");
+                //}
+
+                bool holdLeft = entity.snapLeftAction.GetState(SteamVR_Input_Sources.Any);
+                bool holdRight = entity.snapRightAction.GetState(SteamVR_Input_Sources.Any);
+                if (holdLeft)
+                {
+                    Log.Info("holdLeft");
+                }
+                if (holdRight)
+                {
+                    Log.Info("holdRight");
+                }
+            }
+
         }
     }
 
     public static class VRTouchBtnListenerSystem
     {
+        //private void Pulse()//手柄震动
+        //{
+        //    if (hand && (hand.isActive) && (hand.GetBestGrabbingType() != GrabTypes.None))
+        //    {
+        //        ushort duration = (ushort)Random.Range(minimumPulseDuration, maximumPulseDuration + 1);
+        //        hand.TriggerHapticPulse(duration);
+
+        //        onPulse.Invoke();
+        //    }
+        //}
+
+
 
     }
 
--- Assets/ZFramework/.Hotfix/ClientModel/VRTouchBtnListenerComponent/VRTouchBtnListenerComponent.cs
+++ Assets/ZFramework/.Hotfix/ClientModel/VRTouchBtnListenerComponent/VRTouchBtnListenerComponent.cs
@@ -1,9 +1,12 @@
-﻿using System;
+﻿using KVR_UI;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Valve.VR;
+using Valve.VR.InteractionSystem;
+using Valve.VR.Extras;
 
 namespace ZFramework
 {
@@ -13,5 +16,15 @@ namespace ZFramework
         public SteamVR_Action_Boolean btnB;
         public SteamVR_Action_Boolean btnX;
         public SteamVR_Action_Boolean btnY;
+        public SteamVR_Action_Boolean snapLeftAction;
+        public SteamVR_Action_Boolean snapRightAction;
+
+        public SnapTurn snapTurn;
+
+        public Kvr_InputModule kvr_InputModule;
+        public SteamVR_LaserPointer lineLeft;
+        public SteamVR_LaserPointer lineRight;
+        public Kvr_UIPointer uiPointerLeft;
+        public Kvr_UIPointer uiPointerRight;
     }
 }
