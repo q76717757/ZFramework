f230b3740ad2a3dc9bd220d4b7e07f68b226dfbe
Rucie <76717757@qq.com>
2022-08-30 04:07:41 +0800

    创建server工程  igitignore取消忽略csproj/sln

---
+++ Assets/ZFramework/.Server/Server.App/Boot/BootStrap.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Threading;
+
+namespace ZFramework
+{
+    internal static class BootStrap
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+			//AppDomain.CurrentDomain.UnhandledException += (sender, e) =>
+			//{
+			//	Log.Error(e.ExceptionObject.ToString());
+			//};
+
+			//// 异步方法全部会回掉到主线程
+			//SynchronizationContext.SetSynchronizationContext(ThreadSynchronizationContext.Instance);
+
+
+			//// 命令行参数
+			//Parser.Default.ParseArguments<Options>(args)
+			//	.WithNotParsed(error => throw new Exception($"命令行格式错误! {error}"))
+			//	.WithParsed(Game.AddSingleton);
+
+			//Game.AddSingleton<RandomGenerator>();
+			//Game.AddSingleton<TimeInfo>();
+			//Game.AddSingleton<Logger>().ILog = new NLogger(Options.Instance.AppType.ToString(), Options.Instance.Process, "../Config/NLog/NLog.config");
+			//Game.AddSingleton<ObjectPool>();
+			//Game.AddSingleton<IdGenerater>();
+			//Game.AddSingleton<EventSystem>();
+			//Game.AddSingleton<NetServices>();
+			//Game.AddSingleton<Root>();
+
+			//ETTask.ExceptionHandler += Log.Error;
+
+			//Game.AddSingleton<CodeLoader>().Start();
+
+			//Log.Console($"app start: {Game.Scene.Id} options: {JsonHelper.ToJson(Options.Instance)} ");
+
+			while (true)
+			{
+				try
+				{
+					Thread.Sleep(1);
+					//Game.Update();
+					//Game.LateUpdate();
+				}
+				catch (Exception e)
+				{
+					//Log.Error(e);
+				}
+			}
+		}
+    }
+}
