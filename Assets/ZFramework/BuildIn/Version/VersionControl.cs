63b388e45f3640f698a091c2a5821fa2ccb69777
Rucie <76717757@qq.com>
2022-09-08 16:33:41 +0800

    version定义 以及重写大于小于

--- Assets/ZFramework/BuildIn/Version/VersionControl.cs
+++ Assets/ZFramework/BuildIn/Version/VersionControl.cs
@@ -6,8 +6,17 @@ using UnityEngine;
 //这应该是个独立且统一固定的版本控制逻辑  正常来讲版本控制方案确定之后就不能修改了  否则可能导致版本迭代的过程中因为版本判断不同出现断层
 namespace ZFramework
 {
-    public class Version
+    public class VersionControl
     {
+        public static VersionInfo GetLocalAssemblyVersion()
+        {
+            return default;
+        }
+
+        public static VersionInfo GetRemoteAssemblyVersion()
+        {
+            return default;
+        }
 
 
 
--- Assets/ZFramework/BuildIn/Version/VersionInfo.cs
+++ Assets/ZFramework/BuildIn/Version/VersionInfo.cs
@@ -1,40 +1,53 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Diagnostics.SymbolStore;
 
 namespace ZFramework
 {
+    /// <summary>
+    /// 版本号取四位整数 + 一个字符串后缀(可选)  1.0.1.1-Alpha或者1.0.1.1 都是合法的
+    /// </summary>
     public struct VersionInfo
     {
+        /// <summary>
+        /// 主版本号
+        /// </summary>
         public int X;
+        /// <summary>
+        /// 子版本号
+        /// </summary>
         public int Y;
+        /// <summary>
+        /// 内部版本号
+        /// </summary>
         public int Z;
+        /// <summary>
+        /// 修订版本号
+        /// </summary>
         public int W;
-        public string suffix;
+        /// <summary>
+        /// 版本后缀
+        /// </summary>
+        public string Suf;
 
         public override string ToString()
         {
-            if (string.IsNullOrEmpty(suffix))
+            if (string.IsNullOrEmpty(Suf))
             {
                 return $"{X}.{Y}.{Z}.{W}";
             }
             else
             {
-                return $"{X}.{Y}.{Z}.{W}-{suffix}";
+                return $"{X}.{Y}.{Z}.{W}-{Suf}";
             }
         }
 
-
-        public static bool TryParse(string s,out VersionInfo version)
+        public static bool TryParse(string s, out VersionInfo version)
         {
             if (!string.IsNullOrEmpty(s))
             {
                 string[] parts = s.Split('.');
                 if (parts.Length == 4)
                 {
-                    if (int.TryParse(parts[0],out int x) && 
+                    if (int.TryParse(parts[0], out int x) &&
                         int.TryParse(parts[1], out int y) &&
                         int.TryParse(parts[2], out int z))
                     {
@@ -47,7 +60,7 @@ namespace ZFramework
                                 Y = y,
                                 Z = z,
                                 W = w,
-                                suffix = string.Empty
+                                Suf = string.Empty
                             };
                             return true;
                         }
@@ -58,8 +71,8 @@ namespace ZFramework
                                 X = x,
                                 Y = y,
                                 Z = z,
-                                W= w,
-                                suffix = ex[1]
+                                W = w,
+                                Suf = ex[1]
                             };
                             return true;
                         }
@@ -69,6 +82,26 @@ namespace ZFramework
             version = default;
             return false;
         }
+
+        #region 运算符重载
+        public static bool operator >(VersionInfo v1, VersionInfo v2)
+        {
+            return v1.X > v2.X || v1.Y > v2.Y || v1.Z > v2.Z || v1.W > v2.W;
+        }
+        public static bool operator <(VersionInfo v1, VersionInfo v2)
+        {
+            return v1.X < v2.X || v1.Y < v2.Y || v1.Z < v2.Z || v1.W < v2.W;
+        }
+        public static bool operator >=(VersionInfo v1, VersionInfo v2)
+        {
+            return !(v1 < v2);
+        }
+        public static bool operator <=(VersionInfo v1, VersionInfo v2)
+        {
+            return !(v1 > v2);
+        }
+
+        #endregion
 
     }
 }
