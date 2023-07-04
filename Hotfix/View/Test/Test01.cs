0f122af8b01ee396523ae62a4ec8288dff49a610
Rucie <76717757@qq.com>
2023-07-04 16:30:13 +0800

    [New]新增一个加法运算
    (cherry picked from commit 61f9b7e509c95ab2202cc471462bc946fbe5b3fd)

---
+++ Hotfix/View/Test/Test01.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ZFramework
+{
+    public class Test01 : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+
+        int ADD(int a, int b)
+        {
+            return a - b;
+        }
+    }
+}
