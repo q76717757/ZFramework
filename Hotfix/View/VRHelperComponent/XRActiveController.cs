a7dd64538dcb7cdcf82d8eb1ab13df04ea99610d
Rucie <76717757@qq.com>
2023-07-14 10:12:28 +0800

    [New]添加一个XR头显的切换开关,可用来做PC场景到VR场景的跳转
    (cherry picked from commit 7070386d8b0833540937b951aa20a710c69ed3c7)

---
+++ Hotfix/View/VRHelperComponent/XRActiveController.cs
@@ -0,0 +1,48 @@
+#if ENABLE_XR
+using UnityEngine;
+using UnityEngine.XR.Management;
+
+namespace ZFramework
+{
+    /// <summary>
+    /// 需要先初始化 再Start 头盔才可以显示画面
+    /// 如果你勾选了 ProjectSettings- XR Plug-in Management 中的 Initialize XR On StartUp
+    /// 项目运行时会自动初始化并调用XRManagerSettings中active的XRLoader.Start方法
+    /// </summary>
+    public class XRActiveController
+    {
+        private static XRManagerSettings ManagerSetting => XRGeneralSettings.Instance.Manager;
+        public static bool InitializationComplete => ManagerSetting.isInitializationComplete;
+
+        /// <summary>
+        /// 如果是手动开关XR的,注意结束的时候要把XR关了
+        /// </summary>
+        public static void SetActive(bool state)
+        {
+            if (state)
+            {
+                if (!ManagerSetting.isInitializationComplete)
+                {
+                    ManagerSetting.InitializeLoaderSync();
+                }
+                if (!ManagerSetting.isInitializationComplete)
+                {
+                    Debug.LogError("初始化失败,需要先就绪XR环境");
+                }
+                else
+                {
+                    ManagerSetting.StartSubsystems();
+                }
+            }
+            else
+            {
+                if (ManagerSetting.isInitializationComplete)
+                {
+                    ManagerSetting.StopSubsystems();
+                    ManagerSetting.DeinitializeLoader();
+                }
+            }
+        }
+    }
+}
+#endif
