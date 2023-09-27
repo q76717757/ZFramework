efce80945b24db230018a983eb99799ede7495f1
Rucie <76717757@qq.com>
2023-09-27 11:40:22 +0800

    [new]新增ReservedAssembly,默认引用一些常规dll,可以避免程序集完全未被引用导致整个程序集被裁剪,link无法生效的问题

---
+++ Assembly/Boot/HybridCLR/ReservedAssembly.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+using UnityEngine.Scripting;
+
+namespace ZFramework
+{
+    [Preserve]
+    public class ReservedAssembly : MonoBehaviour
+    {
+        private void Awake()
+        {
+            StringBuilder sb = new StringBuilder();
+
+#if UNITY_STANDALONE_WIN
+            sb.AppendLine(typeof(Microsoft.Win32.SafeHandles.SafeRegistryHandle).ToString());//mscorlib.dll
+            sb.AppendLine(typeof(Microsoft.Win32.IntranetZoneCredentialPolicy).ToString());//System.dll
+            sb.AppendLine(typeof(Microsoft.Win32.SafeHandles.SafePipeHandle).ToString());//System.Core.dll
+#endif
+            sb.AppendLine(typeof(AOT.MonoPInvokeCallbackAttribute).ToString());//UnityEngine.CoreModule.dll
+            sb.AppendLine(typeof(UnityEngine.Event).ToString());//UnityEngine.IMGUIModule.dll
+            sb.AppendLine(typeof(UnityEngine.AndroidJavaRunnable).ToString());//UnityEngine.AndroidJNIModule.dll
+            sb.AppendLine(typeof(UnityEngine.AudioSettings).ToString());//UnityEngine.AudioModule.dll
+            sb.AppendLine(typeof(UnityEngine.UI.AnimationTriggers).ToString()); //UnityEngine.UI.dll
+            sb.AppendLine(typeof(UnityEngine.WWWForm).ToString());//UnityEngine.UnityWebRequestModule.dll
+            sb.AppendLine(typeof(UnityEngine.AssetBundleCreateRequest).ToString());//UnityEngine.AssetBundleModule.dll
+            sb.AppendLine(typeof(UnityEngine.CanvasGroup).ToString());//UnityEngine.UIModule.dll
+            sb.AppendLine(typeof(UnityEngine.TextGenerator).ToString());//UnityEngine.TextRenderingModule.dll
+            sb.AppendLine(typeof(UnityEngine.SharedBetweenAnimatorsAttribute).ToString());//UnityEngine.AnimationModule.dll
+            sb.AppendLine(typeof(UnityEngine.ParticleSystem).ToString());//UnityEngine.ParticleSystemModule.dll
+            sb.AppendLine(typeof(UnityEngine.Playables.PlayableDirector).ToString());//UnityEngine.DirectorModule.dll
+            sb.AppendLine(typeof(UnityEngine.Video.VideoPlayer).ToString());//UnityEngine.VideoModule.dll
+            sb.AppendLine(typeof(UnityEngine.LocationService).ToString());//UnityEngine.InputLegacyModule.dll
+            sb.AppendLine(typeof(UnityEngine.Networking.DownloadHandlerTexture).ToString());//UnityEngine.UnityWebRequestTextureModule.dll
+
+            Debug.Log(sb.ToString());
+        }
+    }
+}
