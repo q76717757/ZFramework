afb4ca3ce5edff31f83516a572e5f32f7a8a464d
Rucie <76717757@qq.com>
2023-10-12 10:56:13 +0800

    [remove] 移除UnityEntry入口方法的冗余行

--- Assembly/View/Entry/UnityEntry.cs
+++ Assembly/View/Entry/UnityEntry.cs
@@ -9,17 +9,6 @@ namespace ZFramework
         public override void OnStart()
         {
             Log.Info("Game Start!");
-
-            Entity entity = new Entity();
-            var child = entity.AddChild();
-            var component = child.AddComponent<TestComponent>();
-            //var component2 = component.GetComponent<SceneComponent>();
-            //var components3 = component2.GetComponentInChildren<SceneComponent>();
-
-            //entity.Parent = null;
-
-            //Component.Destory(component);
-            //Entity.Destory(entity);
         }
     }
 }
