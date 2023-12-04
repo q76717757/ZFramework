1a3f4162a95135f3098544ef3e7a7d62a790c0b5
Rucie <76717757@qq.com>
2023-12-04 10:54:37 +0800

    [new] 添加一个简易的UI组件
    (cherry picked from commit 46abac6d7154d6cd9958b6b76173cb4c4f8c590c)

---
+++ Assembly/View/Components/UIComponent/UIPathAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZFramework
+{
+    public class UIPathAttribute :BaseAttribute
+    {
+        public string Path { get; }
+        public UIPathAttribute(string path)
+        {
+            Path = path;
+        }
+    }
+}
